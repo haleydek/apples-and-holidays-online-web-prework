096957c8256cab9470fe1efa0224b4db4a927b5c
Haley DeKeyser <hcd09@live.com>
2019-05-12 22:37:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,7 +95,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     level_one = "#{season}:"
     level_one.capitalize
-    level one
+    level_one
   end
 end
 
